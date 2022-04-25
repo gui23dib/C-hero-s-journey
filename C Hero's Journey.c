6631fefb4776dacb6dc1aead772a0e3de8f1472d
Guilherme Dib <gui23dib@gmail.com>
2022-04-24 23:13:05 -0300

    Combate com inputs de recompensa final

--- C Hero's Journey.c
+++ C Hero's Journey.c
@@ -17,20 +17,63 @@ int main(){
     player.str = 1;
     player.con = 1;
     player.hp = 6 + player.con;
-    enemy.dex = 1;
-    enemy.str = 1;
-    enemy.con = 1;
-    enemy.hp = 4 + enemy.con;
 
+    int points = 0; //TOTAL DE DANO CAUSADO
+
+    int battles = 0;
+    for(;;){ //LOOP INFINITO PARA CONTINUIDADE DO JOGO
+    battles ++;
+
+
+    printf("============== INIMIGO %i ==============\n\b", battles);
+    switch (battles){
+    case 1:
+        enemy.dex = 1;
+        enemy.str = 1;
+        enemy.con = 1;
+        enemy.hp = 4 + enemy.con;
+        break;
+    case 2:
+        enemy.dex = 2;
+        enemy.str = 0;
+        enemy.con = 4;
+        enemy.hp = 6 + enemy.con;
+        break;
+    case 3:
+        enemy.dex = 3;
+        enemy.str = -1;
+        enemy.con = 0;
+        enemy.hp = 6 + enemy.con;
+        break;
+    case 4:
+        enemy.dex = 2;
+        enemy.str = 2;
+        enemy.con = 3;
+        enemy.hp = 3 + enemy.con;
+        break;
+    case 5:
+        enemy.dex = 4;
+        enemy.str = 1;
+        enemy.con = 2;
+        enemy.hp = 4 + enemy.con;
+        break;
+    case 6:
+        enemy.dex = 6;
+        enemy.str = 6;
+        enemy.con = 6;
+        enemy.hp = 4 + enemy.con;
+        break;
+    default:
+        enemy.dex = 6 + battles;
+        enemy.str = 6 + battles;
+        enemy.con = 6 + battles;
+        enemy.hp = 6 + enemy.con + battles;
+        break;
+
+    }
 
     int turn = 1; //VAR DE CONTAGEM DE TURNOS
     while(enemy.hp>0){
-
-        if(player.hp <= 0){ //MORTE DO PLAYER
-            printf("\n\nVoce morreu, fim da historia...\n");
-            printf("Finalizando jogo...\n");
-            return 0;
-        }
 
         printf("\n======== TURNO %i ========\n\n", turn);
         turn++; //CONTADOR DE TURNOS
@@ -41,6 +84,19 @@ int main(){
             damage = ((rand()%6)+1) + player.str; //CONTAGEM DE DANO
             printf("Voce acerta o 'goblin' causando %i de dano!\n", damage);
             enemy.hp -= damage; //SUBTRACAO DA VIDA
+            points += damage; //ADICAO DOS PONTOS
+
+            if((rand()%6)+1<=enemy.dex){ //ACERTO DO INIMIGO
+
+                damage = ((rand()%6)+1) + enemy.str; //CONTAGEM DE DANO
+                printf("O 'goblin' acerta causando %i de dano!\n", damage);
+                player.hp -= damage; //SUBTRACAO DA VIDA
+
+            } else { //ERRO DO INIMIGO
+
+                printf("'Goblin' errou!\n");
+
+            }
 
         } else { //ERRO DO GOLPE
 
@@ -60,7 +116,13 @@ int main(){
         }
 
         printf("\nHP ATUAL: %i\n", player.hp);
-        printf("HP GOBLIN: %i\n", enemy.hp);
+        printf("HP INIMIGO: %i\n", enemy.hp);
+
+          if(player.hp <= 0){ //MORTE DO PLAYER
+            printf("\n\nVoce morreu com %i pontos, fim da historia...\n", points);
+            printf("Finalizando jogo...\n");
+            return 0;
+        }
     }
 
 
@@ -77,16 +139,16 @@ int main(){
 
     switch (p_decision){
     case 1:
-        player.str += ((rand()%3)+1);
+        player.str += 1;
         break;
     case 2:
-        player.dex += ((rand()%3)+1);
+        player.dex += 1;
         break;
     case 3:
-        player.con += ((rand()%3)+1);
+        player.con += 1;
         break;
     case 4:
-        player.hp += ((rand()%3)+1);
+        player.hp += ((rand()%5+1));
         break;
     } //MUDANCA DAS VARIAVES DE ATT
 
@@ -97,4 +159,9 @@ int main(){
     scanf("%i", &p_decision);
     if(p_decision == 2)
         return 0;
+
+    }
+
+
+return 0;
 }
